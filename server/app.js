ec0a837ac585d524bf98939867a3e254e68fe165
YPZ-24 <aylinypz@gmail.com>
2021-03-22 01:39:13 -0600

    basico 2

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import questionRouter from './routes/question';
 import errorMiddleware from './util/errorMiddleware';
+import cors from 'cors';
 const app = express();
 
 //MIDDLEWARES
 app.use(express.json());
+app.use(cors());
 
 //ROUTES
 app.use(questionRouter)

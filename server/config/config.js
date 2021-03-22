1f859c6542bf8dee7aae535c20140f67d01257dd
YPZ-24 <aylinypz@gmail.com>
2021-03-22 07:46:55 -0600

    trying

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 export default {
-    PORT: process.env.PORT || 8000,
+    PORT: process.env.PORT,
     DB:{
         //URL: process.env.DB_URL || 'mongodb://localhost:27017/askme',
         URL: 'mongodb+srv://root:toor@cluster0.xv7op.mongodb.net/askme?',
